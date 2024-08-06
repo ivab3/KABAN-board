af12ac01f9611eab10430aad433859d29af68683
ivabe <ivanberdyugin9@gmail.com>
2024-08-07 02:23:46 +0500

    hello world commit vol2

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main className="bg-[#E1E8ED]  flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="text-black z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                zdarova
+            </div>
+        </main>
+    );
+}
